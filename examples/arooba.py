b15bc1f0b72d4e9dd4fe13d9465c4096976a6d98
aroobaalfarsi <aroobaalfarsi@gmail.com>
2025-04-24 12:26:54 +0400

    trangle test

--- examples/arooba.py
+++
@@ -1,8 +0,0 @@
-# Get input from the user
-rows = int(input("Enter the number of rows: "))
-
-# Loop to generate the pattern
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print(j, end='')
-    print()  # Newline after each row
