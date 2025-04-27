42327641a7fd53de1744f7f62a12ac6dbb8023ed
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-04-27 12:02:51 +0400

    changed a few things

--- examples/from_Tariq/Tic-Tac-Toe.py
+++ examples/from_Tariq/Tic-Tac-Toe.py
@@ -3,20 +3,41 @@ import time
 import random
 
 def check_winner(board):
- for row in board:
-    if row.count(row[0]) == len(row) and row[0] in ['X', 'O']:
-        return row[0]
- for col in range(3):
-    if board[0][col] == board[1][col] == board[2][col] and board[0][col] in ['X', 'O']:
-        return board[0][col]
- if board[0][0] == board[1][1] == board[2][2] and board[0][0] in ['X', 'O']:
-    return board[0][0]
- if board[0][2] == board[1][1] == board[2][0] and board[0][2] in ['X', 'O']:
-    return board[0][2]
- return None
+    if board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X':
+        return 'X'
+    if board[0][0] == 'O' and board[0][1] == 'O' and board[0][2] == 'O':
+        return 'O'
+    if board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X':
+        return 'X'
+    if board[1][0] == 'O' and board[1][1] == 'O' and board[1][2] == 'O':
+        return 'O'
+    if board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X':
+        return 'X'
+    if board[2][0] == 'O' and board[2][1] == 'O' and board[2][2] == 'O':
+        return 'O'
+    if board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X':
+        return 'X'
+    if board[0][0] == 'O' and board[1][0] == 'O' and board[2][0] == 'O':
+        return 'O'
+    if board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X':
+        return 'X'
+    if board[0][1] == 'O' and board[1][1] == 'O' and board[2][1] == 'O':
+        return 'O'
+    if board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X':
+        return 'X'
+    if board[0][2] == 'O' and board[1][2] == 'O' and board[2][2] == 'O':
+        return 'O'
+    if board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
+        return 'X'
+    if board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O':
+        return 'O'
+    if board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
+        return 'X'
+    if board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O':
+        return 'O'
+    return None
 
 
-y = 0
 playerstupid = 0
 tictactoe = [
       [1,2,3],
@@ -32,9 +53,9 @@ game_active = True
 while game_active:
    x == str(x)
    print("you will play as "+ x)
-   time.sleep(2)
+   time.sleep(1)
    print("pls pick a number from the grid to proced")
-   time.sleep(2)
+   time.sleep(1)
    print(tabulate(tictactoe, tablefmt='fancy_grid'))
 
 
@@ -78,11 +99,19 @@ while game_active:
      playerstupid = 0
     else:
      botstupid = 0
-     
 
-   if all(cell in ['X', 'O'] for row in tictactoe for cell in row):
-       print("It's a draw!")
-       game_active = False 
+is_draw = True
+for row in tictactoe:
+ for cell in row:
+        if cell != 'X' and cell != 'O':  # If we find any number left
+            is_draw = False
+            break
+ if not is_draw:
+        break
+
+ if is_draw:
+    print("It's a draw!")
+    game_active = False
 
 
 
