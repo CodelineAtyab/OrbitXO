34cb07dd4f4fc7b6cb9e45ae310a07cdc311398c
s99ss <alghilanisuhaila@gmail.com>
2025-04-27 14:06:04 +0400

    Feature/86et8cwzp row number (#12)
    * feature/86et8cwzp-no-pattern
    
    * feature/86et8cwzp-row-number
    
    * feature/86et8cwzp-row-number
    
    * feature/86et8cwzp-row-number

---
+++ examples/from_suhaila/num_of_rows.py
@@ -0,0 +1,8 @@
+num_of_rows = int(input("Enter number of rows: "))
+
+# Generate triangle pattern
+for i in range(1, num_of_rows + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+    
