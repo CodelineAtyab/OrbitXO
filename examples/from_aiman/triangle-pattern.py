3c4c62c606ab9ebfdf7ccafd160548534a9a662a
Aiman-Amin <aimanamin101@gmail.com>
2025-04-27 10:38:50 +0400

    triangle pattern

---
+++ examples/from_aiman/triangle-pattern.py
@@ -0,0 +1,18 @@
+def print_triangle_pattern(num_of_rows):
+    number = 1
+    for row in range(1, num_of_rows + 1):
+        for col in range(1, row + 1):
+            print(col, end=" ")
+            
+        print()  # Move to the next line after each row
+
+if __name__ == "__main__":
+    try:
+        num_of_rows = int(input("Enter the number of rows: "))
+        if num_of_rows < 1:
+            print("Please enter a positive integer greater than 0.")
+        else:
+            print("\nGenerated Triangle Pattern:\n")
+            print_triangle_pattern(num_of_rows)
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
