6301adca94fb86bcd513d8996f8825801216fb9b
Tufool98 <Tufool.hn@hotmail.com>
2025-04-29 15:02:11 +0400

    tictactoegame

--- examples/from_tufool/tufool_exercise.py
+++ examples/from_tufool/tufool_exercise.py
@@ -1,101 +1,74 @@
-import random
-
-# Initialize board
-board = [str(i) for i in range(1, 10)]
-
-# Welcome message and symbol selection
-print("Welcome to Tic Tac Toe!")
-player_symbol = input("Choose your symbol (X or O): ").upper()
-
-while player_symbol != 'X' and player_symbol != 'O':
-    player_symbol = input("Invalid choice. Please choose X or O: ").upper()
-
-bot_symbol = 'O' if player_symbol == 'X' else 'X'
-print(f"You are {player_symbol}, the bot is {bot_symbol}. Let's start!")
-
-# Print board
+# Initialize board (1-9)
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# Asking the player to choose X or O
+player = ""
+bot = ""
+while player != "X" and player != "O":
+    player = input("Choose X or O: ").upper()
+if player == "X":
+    bot = "O"
+else:
+    bot = "X"
+# Show the board
 print()
-print(f"{board[0]} | {board[1]} | {board[2]}")
-print("--+---+--")
-print(f"{board[3]} | {board[4]} | {board[5]}")
-print("--+---+--")
-print(f"{board[6]} | {board[7]} | {board[8]}")
+print(" " + board[0] + " | " + board[1] + " | " + board[2])
+print("---+---+---")
+print(" " + board[3] + " | " + board[4] + " | " + board[5])
+print("---+---+---")
+print(" " + board[6] + " | " + board[7] + " | " + board[8])
 print()
-
-# Game loop
+# Start the game
 game_over = False
-
-while not game_over:
-    # --- Player Move ---
-    valid_move = False
-    while not valid_move:
-        try:
-            move = int(input("Your move (1-9): "))
-            if move < 1 or move > 9:
-                print("Please enter a number between 1 and 9.")
-            elif board[move - 1] in ['X', 'O']:
-                print("Spot already taken. Choose another one.")
+turn = "player"
+while game_over == False:
+    if turn == "player":
+        move = input("Choose a position (1-9): ")
+        if move == "1" or move == "2" or move == "3" or move == "4" or move == "5" or move == "6" or move == "7" or move == "8" or move == "9":
+            move = int(move) - 1
+            if board[move] != "X" and board[move] != "O":
+                board[move] = player
+                turn = "bot"
             else:
-                board[move - 1] = player_symbol
-                valid_move = True
-        except:
-            print("Invalid input. Please enter a number.")
-
-    # Print board
+                print("That spot is already taken.")
+        else:
+            print("Invalid input. Type a number between 1 and 9.")
+    else:
+        print("It is bot turn...")
+        bot_move_done = False
+        i = 0
+        while bot_move_done == False and i < 9:
+            if board[i] != "X" and board[i] != "O":
+                board[i] = bot
+                bot_move_done = True
+                turn = "player"
+            i = i + 1
+    # Show the board after each move
     print()
-    print(f"{board[0]} | {board[1]} | {board[2]}")
-    print("--+---+--")
-    print(f"{board[3]} | {board[4]} | {board[5]}")
-    print("--+---+--")
-    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("---+---+---")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("---+---+---")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
     print()
-
-    # Check for win
-    win_conditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ]
-    for condition in win_conditions:
-        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player_symbol:
-            print("Congratulations! You win!")
-            game_over = True
-            break
-
-    if game_over:
-        break
-
+    # Check for a win
+    if (board[0] == board[1] == board[2] and (board[0] == "X" or board[0] == "O")) or \
+       (board[3] == board[4] == board[5] and (board[3] == "X" or board[3] == "O")) or \
+       (board[6] == board[7] == board[8] and (board[6] == "X" or board[6] == "O")) or \
+       (board[0] == board[3] == board[6] and (board[0] == "X" or board[0] == "O")) or \
+       (board[1] == board[4] == board[7] and (board[1] == "X" or board[1] == "O")) or \
+       (board[2] == board[5] == board[8] and (board[2] == "X" or board[2] == "O")) or \
+       (board[0] == board[4] == board[8] and (board[0] == "X" or board[0] == "O")) or \
+       (board[2] == board[4] == board[6] and (board[2] == "X" or board[2] == "O")):
+        game_over = True
+        if turn == "bot":
+            print("You win! CONGRATULATIONS!")
+        else:
+            print("OPS! Bot wins!")
     # Check for draw
-    if all(spot in ['X', 'O'] for spot in board):
+    empty_spaces = 0
+    for j in range(9):
+        if board[j] != "X" and board[j] != "O":
+            empty_spaces = empty_spaces + 1
+    if empty_spaces == 0 and game_over == False:
         print("It's a draw!")
-        break
-
-    # --- Bot Move ---
-    available_moves = [i for i in range(9) if board[i] not in ['X', 'O']]
-    bot_move = random.choice(available_moves)
-    board[bot_move] = bot_symbol
-    print(f"Bot chose position {bot_move + 1}")
-
-    # Print board
-    print()
-    print(f"{board[0]} | {board[1]} | {board[2]}")
-    print("--+---+--")
-    print(f"{board[3]} | {board[4]} | {board[5]}")
-    print("--+---+--")
-    print(f"{board[6]} | {board[7]} | {board[8]}")
-    print()
-
-    # Check if bot wins
-    for condition in win_conditions:
-        if board[condition[0]] == board[condition[1]] == board[condition[2]] == bot_symbol:
-            print("Bot wins! Better luck next time.")
-            game_over = True
-            break
-
-    if game_over:
-        break
-
-    # Check for draw again
-    if all(spot in ['X', 'O'] for spot in board):
-        print("It's a draw!")
-        break
+        game_over = True
