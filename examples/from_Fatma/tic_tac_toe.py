bb2797ded1175651a36fd5b1916d3f304bd84ad1
FatmaMohammed9 <fatoomzz9911@gmail.com>
2025-04-30 15:31:30 +0400

    Final

--- examples/from_Fatma/tic_tac_toe.py
+++ examples/from_Fatma/tic_tac_toe.py
@@ -1,12 +1,9 @@
 
-board = [str(i + 1) for i in range(9)]
-
+# Initial board with numbers 1 to 9
+board = ['1','2','3','4','5','6','7','8','9']
+# Welcome and symbol selection
 print("Welcome to Tic Tac Toe!")
-print(" 1 | 2 | 3 ")
-print("---+---+---")
-print(" 4 | 5 | 6 ")
-print("---+---+---")
-print(" 7 | 8 | 9 ")
+
 player = input("Choose X or O: ").upper()
 if player == "X":
     bot = "O"
@@ -15,8 +12,9 @@ else:
     bot = "X"
 turn = "Player"
 game_over = False
-
+# Start the game loop
 while not game_over:
+    # Display the board
     print()
     print(" " + board[0] + " | " + board[1] + " | " + board[2])
     print("---+---+---")
@@ -24,16 +22,13 @@ while not game_over:
     print("---+---+---")
     print(" " + board[6] + " | " + board[7] + " | " + board[8])
     print()
-
-    if all(cell in ["X", "O"] for cell in board):
-        print("It's a draw!")
-        break
+    
     if turn == "Player":
         try:
             pos = int(input("Enter position (1-9): ")) - 1
             if board[pos] not in ["X", "O"]:
                 board[pos] = player
-
+                # Check if player wins
                 if (board[0] == board[1] == board[2] == player or
                     board[3] == board[4] == board[5] == player or
                     board[6] == board[7] == board[8] == player or
@@ -63,7 +58,7 @@ while not game_over:
             if board[i] not in ["X", "O"]:
                 board[i] = bot
                 break
-
+        # Check if bot wins
         if (board[0] == board[1] == board[2] == bot or
             board[3] == board[4] == board[5] == bot or
             board[6] == board[7] == board[8] == bot or
@@ -83,3 +78,7 @@ while not game_over:
             game_over = True
         else:
             turn = "Player"
+    # Check for draw
+    if " " not in board:
+        print("It's a draw!")
+        break
