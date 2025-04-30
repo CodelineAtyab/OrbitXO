a2d82748a729e9a579d5166f7c66d9a849f2eee3
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-04-30 10:06:27 +0400

    Added DRY example for funtions

--- examples/from_atyab/func_prac.py
+++ examples/from_atyab/func_prac.py
@@ -1,14 +1,29 @@
-print("Starting the program")
+import time
 
-# WE ARE JUST DEFINING A FUNCTION AND NOT USING/CALLING IT
-# A Function uses the same formula on different data and processes it and returns an output value
-# num1, num2 are function parameters (input to the function)
-def calc_sum(num_1, num_2):  # Function definition
-  a = num_1 + num_2  # Processing the inputs, arguments
-  return a  # Returning the output value
+# DRY Principle (D)on't (R)epeat (Y)ourself and use functions
+def greet_vips(vip_name_1, vip_name_2, vip_name_3):
+  print("Greetings " + vip_name_1 + ", " + vip_name_2 + ", " + vip_name_3)
+  print("Welcome to Technopark")
+  print("Its so good to see all of you here")
+  print("***********************************\n")
+  time.sleep(5)
 
-# Substitute the return value with calc_sum(5, 10)
-result = calc_sum(5, 10)
 
-print("Result of Sum is: ", result)
-print("Ending the program")
+name_of_vip_guest_1 = "Mr.Ahmed"
+name_of_vip_guest_2 = "Mr.Muhannad"
+name_of_vip_guest_3 = "Ms.Sohaila"
+
+# Greetings message
+greet_vips(name_of_vip_guest_1, name_of_vip_guest_2, name_of_vip_guest_3)
+
+name_of_vip_guest_1 = "Ms.Aya"
+name_of_vip_guest_2 = "Ms.Maria"
+name_of_vip_guest_3 = "Ms.Hanan"
+
+greet_vips(name_of_vip_guest_1, name_of_vip_guest_2, name_of_vip_guest_3)
+
+name_of_vip_guest_1 = "Ms.Arooba"
+name_of_vip_guest_2 = "Ms.Sundus"
+name_of_vip_guest_3 = "Ms.Ghadeer"
+
+greet_vips(name_of_vip_guest_1, name_of_vip_guest_2, name_of_vip_guest_3)
