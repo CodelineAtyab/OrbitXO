f86dc0408244fd2d7ddbacb4c540d1a9347c7297
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-04-30 14:52:28 +0400

    changed from list of sets to list of list

--- examples/from_Tariq/friend-comparison.py
+++ examples/from_Tariq/friend-comparison.py
@@ -20,10 +20,10 @@ while mainmenu:
             friend = input("Enter friend (comma-separated): ")
             friend_list = []
             friend_list = friend.split(",")
-            friend_set = set(friend_list)
+            friend_set = friend_list
             if user in all_friends:
                 print("User already exists. Adding friend to existing user.")
-                all_friends[user].add(friend_set)
+                all_friends[user].extend(friend_set)
             else:
                 print("Adding new user and friend.")
                 all_friends[user] = []
@@ -36,8 +36,12 @@ while mainmenu:
             print("Find mutual friends.")
             user1 = input("Enter first user: ")
             user2 = input("Enter second user: ")
-            user1_sets = set().union(*all_friends[user1])
-            user2_sets = set().union(*all_friends[user2])
+            user1_sets = set()
+            user2_sets = set()
+            for friend_set in all_friends[user1]:
+                user1_sets = user1_sets | set(friend_set)
+            for friend_set in all_friends[user2]:
+                user2_sets = user2_sets | set(friend_set)
             mutual_friends = user1_sets & user2_sets
             if not mutual_friends :
              print("No mutual friends found.")
@@ -48,8 +52,12 @@ while mainmenu:
             print("Find unique friends.")
             user1 = input("Enter first user: ")
             user2 = input("Enter second user: ")
-            user1_sets = set().union(*all_friends[user1])
-            user2_sets = set().union(*all_friends[user2])
+            user1_sets = set()
+            user2_sets = set()
+            for friend_set in all_friends[user1]:
+                user1_sets = user1_sets | set(friend_set)
+            for friend_set in all_friends[user2]:
+                user2_sets = user2_sets | set(friend_set)
             unique_friends = user1_sets - user2_sets
             if not unique_friends:
              print("No unique friends found.")
@@ -60,8 +68,12 @@ while mainmenu:
             print("Combine all friends.")
             user1 = input("Enter first user: ")
             user2 = input("Enter second user: ")
-            user1_sets = set().union(*all_friends[user1])
-            user2_sets = set().union(*all_friends[user2])
+            user1_sets = set()
+            user2_sets = set()
+            for friend_set in all_friends[user1]:
+                user1_sets = user1_sets | set(friend_set)
+            for friend_set in all_friends[user2]:
+                user2_sets = user2_sets | set(friend_set)
             combined_friends = user1_sets | user2_sets
             if not combined_friends:
              print("No combined friends found.")
@@ -71,19 +83,23 @@ while mainmenu:
         elif x == '5':
             print("Get friend suggestions.")
             user = input("Enter user: ")
-            user_friends = set().union(*all_friends[user])
+            user_sets = set()
             suggestions = set()
+            for friend_set in all_friends[user]:
+             user_sets = user_sets | set(friend_set)
             for other_user, friend_sets in all_friends.items():
              if other_user != user:
-                 other_user_friends = set().union(*friend_sets)
-                 mutual_friends = user_friends & other_user_friends      
-                 if mutual_friends:
-                  suggestions.add(other_user)  
-                  suggestions.update(other_user_friends - user_friends)     
+              other_user_sets = set()
+              for friend_set in friend_sets:
+               other_user_sets = other_user_sets | set(friend_set)
+               mutual_friends = user_sets & other_user_sets
+               if mutual_friends:
+                suggestions.add(other_user)
+                suggestions.update(other_user_sets - user_sets)
             if not suggestions:
-             print("No friend suggestions found.")
+                print("No friend suggestions found.")
             else:
-             print("Friend suggestions:", suggestions)
+                print("Friend suggestions:", suggestions)
             break
         elif x == '6':
             print("Exiting the program.")
