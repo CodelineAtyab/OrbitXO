2ff122cac5bbcd743ed702880f355ba6c07f74b4
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-05-01 09:05:48 +0400

    finish tell option 2 , still in progress tho

--- examples/from_almuhannad/friend_comparison.py
+++ examples/from_almuhannad/friend_comparison.py
@@ -8,26 +8,38 @@ while True:
     print("6. Exit")
 
 
-    user_friends_mapping = {}
+    user_friends = {} #dictonary 
 
     a = input("Choose an option: ")
     if a == "1":
             print("Add user and friends \n")
-            entName = input("Enter user name: ")
-            entFriends = input("Enter friends (comma-separated): ")
-            entFriends.split(",")
-            print(entFriends)
-            user_friends_mapping[entName] = entFriends
+            entName = input("Enter user name: ") # Entering a username 
+            entFriends = input("Enter friends (comma-separated): ") # entering friends (ahmed,ayman,tariq)
+            friend_list = [] # create a new list to store the friends name.
+            friend_list = entFriends.split(",") #split each name ( ahmed ayman tariq)
+            if entName in user_friends:  # it will check if the username is already in the dictonary
+                   print("User Already Exists. ")
+                   user_friends[entName].extend(friend_list) #extend takes each item in the list and adds it one-by-one to the existing list.
+            else:
+                   print("Adding new user and friends.")
+                   user_friends[entName] = [] # will create an empty list for the username for example user_friends["Ali"] = []
+                   user_friends[entName].append(friend_list) #This adds the entire list friend_list as a single item inside the user's list.
+                   print("User and friend added successfully.")
+                   print("Friend set: ", friend_list)
+                   print("All users: ", user_friends[entName])
             
-    elif a == "2":
-            print("Find ur mutual friends")
-            first_name = input("Enter first user: ")
-            secound_name = input("Enter Secound user: ")
-            print(first_name)
-            print(secound_name)
-            mutual_friends = entFriends.intersection()
-            print("Mutual friends: ", mutual_friends )
-            s = set()
-            s.add("")
+            if a == "2":
+                print("Find ur mutual friends")
+                first_name = input("Enter first user: ")
+                secound_name = input("Enter Secound user: ")
+                first_name_set = set()
+                secound_name_set = set()
+                for friend_list in user_friends[first_name]:
+                      first_name_set = first_name_set | set(friend_list)
+                for friend_list in user_friends[secound_name]:
+                      secound_name_set = secound_name_set | set(friend_list)
+
+                mutual_friends = first_name_set & secound_name_set
+                print("Mutual friends: ", mutual_friends )
             
 
