ed4b42423f4817d4e00b51579531b0263e1bc614
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-05-04 10:11:57 +0400

    changed few things, now done

--- examples/from_almuhannad/track_students_grade.py
+++ examples/from_almuhannad/track_students_grade.py
@@ -18,26 +18,49 @@ while True:
         print(list_of_grades)
 
     elif a == "2":
-        print("Remove Grade")
-        print(list_of_grades)
-        grade_to_remove = int(input("enter a grade to remove: "))
-        if grade_to_remove in list_of_grades:
-            list_of_grades.remove(grade_to_remove)
-            print("Grade is removed ",grade_to_remove)
-        else:
-             print("Grade not found!")
+        print("1. Enter a grade to remove")
+        print("2. Enter index to remove")
+        
+        c = input("Enter a chooice: ")
+
+        if c == "1":
+
+            print(list_of_grades)
+            grade_to_remove = int(input("Enter a grade to remove: "))
+            if grade_to_remove in list_of_grades:
+                list_of_grades.remove(grade_to_remove)
+                print("Grade is removed ",grade_to_remove)
+            else:
+                print("Grade not found!")
+
+        elif c == "2":
+            print(list_of_grades)
+            index_to_remove = int(input("Enter index to remove: "))
+            if 0 <= index_to_remove < len(list_of_grades):
+                rindex = list_of_grades.pop(index_to_remove)
+                print("Grade index have been removed")
+            else:
+                print("Index not found")
         print(list_of_grades)
 
     elif a == "3":
         print("show statistics: ")
-        print(min(list_of_grades))
-        print(max(list_of_grades))
+        print("Minimum: ", min(list_of_grades))
+        print("Maximum: ", max(list_of_grades))
+        print("Average: ", sum(list_of_grades)/len(list_of_grades))
         print(list_of_grades)
-        print(sum(list_of_grades))
 
     elif a == "4":
-        print("Sorting Grades")
-        print(sorted(list_of_grades))
+        print("1. Sort by Asending")
+        print("2. Sort by Descending")
+        x = input("Enter ur choice: ")
+
+        if x == "1":
+            list_of_grades.sort()
+            print(list_of_grades)
+        elif x == "2":
+            list_of_grades.sort(reverse=True)
+            print(list_of_grades)
 
     elif a == "5":
         print(list_of_grades)
