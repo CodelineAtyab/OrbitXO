c910c43fe3872d691725b7c00801f2c98d1b7113
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-05-12 10:56:02 +0400

    fixed char limit

--- examples/from_Tariq/character-limit-enforcement.py
+++ examples/from_Tariq/character-limit-enforcement.py
@@ -1,7 +1,7 @@
 msg = input("write your message: ")
 if len(msg) > 280:
     print("your message exceeds the character limit of 280")
-    nmsg = msg[0:281] + "..."
+    nmsg = msg[0:280] + "..."
     print("your message has been truncated to: ", nmsg)
 else:
     print("your message is within the character limit of 280")
