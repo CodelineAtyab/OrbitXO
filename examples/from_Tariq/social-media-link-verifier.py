391f23ca1df2d3e3b16a1aa25d1320817baa0097
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-05-13 11:58:19 +0400

    fixed the logic

--- examples/from_Tariq/social-media-link-verifier.py
+++ examples/from_Tariq/social-media-link-verifier.py
@@ -1,5 +1,9 @@
 msg = input("write valid URL: ")
-if msg.startswith("https://") or msg.startswith("http://") and "." in msg[8:]:
+if msg.startswith("https://"): 
+    x=8
+elif msg.startswith("http://"):
+    x=7 
+if (msg.startswith("https://") or msg.startswith("http://")) and "." in msg[x:]:
     domaindot = msg.find(".")
     if domaindot > 0 and domaindot < len(msg) - 1:
          print("Valid URL")
