7226bf493d78977fcabb135661de2d6beda681b2
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-13 15:16:25 +0400

    interactive scripts and pros and cons

---
+++ examples/from_atyab/project_orbit/non_interactive_calc.py
@@ -0,0 +1,11 @@
+import sys
+from yousif_calc import calc_sum
+
+# Catch external arguments from the CLI
+print(sys.argv)
+
+name_1 = sys.argv[1] # 2nd Item is Arooba
+name_2 = sys.argv[2] # 3rd Item is Aya
+
+print(f"Hello {name_1} and {name_2}")
+
