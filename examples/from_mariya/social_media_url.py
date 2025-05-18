69e669b067996e755c0f33fdb27f04d030fddc35
Mariya-00 <mariya.rashid20@outlook.com>
2025-05-18 15:44:00 +0400

    URL

---
+++ examples/from_mariya/social_media_url.py
@@ -0,0 +1,11 @@
+print("\nWelcome to URL task :)")
+
+user_url = input("Please type your link here: ")
+
+if (user_url.startswith("http://") or user_url.startswith("https://")):
+    if "." in user_url:
+        print("True URL .. well done :) ")
+    else:
+        print("Invalid URL (no dot found)")
+else:
+    print("Invalid URL (must start with http:// or https://)")
