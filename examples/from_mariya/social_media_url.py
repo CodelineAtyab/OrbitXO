5c593ba61c419399af36b43ad989f3ca8972a9f8
Mariya-00 <mariya.rashid20@outlook.com>
2025-05-18 16:24:23 +0400

    adding the not endswith

--- examples/from_mariya/social_media_url.py
+++ examples/from_mariya/social_media_url.py
@@ -3,7 +3,7 @@ print("\nWelcome to URL task :)")
 user_url = input("Please type your link here: ")
 
 if (user_url.startswith("http://") or user_url.startswith("https://")):
-    if "." in user_url:
+    if "." in user_url and not user_url.endswith("."):
         print("True URL .. well done :) ")
     else:
         print("Invalid URL (no dot found)")
