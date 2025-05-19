c31bb1bc80ac2fb0353126d7d255448f34970b20
HananBlc9 <hanan.balucci@gmail.com>
2025-05-19 09:43:10 +0400

    updated link virifier

--- examples/from_Hanan_Balushi/linkVerifier.py
+++ examples/from_Hanan_Balushi/linkVerifier.py
@@ -3,9 +3,11 @@ import re
 file = open("examples/from_Hanan_Balushi/urls.txt", "r")
 for line in file.readlines():
     url = line.strip()
-    if url.startswith("http://") or url.startswith("https://"):
-        if re.search(r"\.[a-zA-Z]+", url):
+    if url.startswith("http://") or url.startswith("https://"): #check if begining is correct
+        if "." in url and not url.endswith(".") and len(url[url.index("."):])>2: # check if here is domain after .
             print(f"URL: {url} = Valid URL!")
+        else:
+            print(f"URL: {url} = Invalid URL!!!") #if beginning is correct but there is no domain print invalid
     else:
         print(f"URL: {url} = Invalid URL!!!")
 file.close()
