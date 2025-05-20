d3435808fce6fe7c5bff3cd264e6b525833be966
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-05-20 09:09:54 +0400

    done with 86etgm7u5 Shopping List Manager: Item Count

---
+++ examples/from_Tariq/shopping-list-manager.py
@@ -0,0 +1,4 @@
+shopping_list = input("Enter your shopping list (comma-separated): ")
+shopping_list = shopping_list.split(",")
+num_of_items = len(shopping_list)
+print(f"Number of items in the shopping list: {num_of_items}")
