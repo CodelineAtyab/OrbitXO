5519ecbf1c5c7d7a7ee05b2e5014919d0e7cccef
Mariya-00 <mariya.rashid20@outlook.com>
2025-05-21 15:53:44 +0400

    shopping list1

---
+++ examples/from_mariya/shopping_list.py
@@ -0,0 +1,5 @@
+def count_list(shopping_list):
+    return len(shopping_list)
+
+my_list = ["milk", "banana", "pineapple", "apple", "bread", "eggs", "apples"]
+print("Total items:", count_list(my_list))
