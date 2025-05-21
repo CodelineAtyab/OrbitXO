20e1d1c2f990cfc70710086eddcb31b102e7b849
Hoor-00 <alkaabihoor4@gmail.com>
2025-05-21 15:18:41 +0400

    shopping-list

---
+++ examples/from_hoor/shopping_list.py
@@ -0,0 +1,9 @@
+def count_items(shopping_list: list[str]):
+    # shopping_list=[]
+    if len(shopping_list) == 0:
+        return 0
+    else:
+       
+        return len(shopping_list)
+    
+print(count_items(["apple","banana","juice"]))
