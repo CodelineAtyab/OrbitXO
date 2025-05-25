f24a1781b5395de40e64dcd60f866a16c8c14181
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-05-25 13:06:07 +0400

    Done with simple email stracture

--- examples/from_almuhannad/simple_email_stracture.py
+++ examples/from_almuhannad/simple_email_stracture.py
@@ -1,12 +1,12 @@
 user_email = input("Enter your email: ")
 
 if "@" in user_email and "." in user_email and\
+   user_email.count("@") == 1 and\
    user_email.index("@") > 0 and\
    user_email.index("@") < len(user_email)-1 and\
-   user_email.index(".") > user_email.index("@") and\
-   len(user_email[user_email.rindex(".")+1]) > 2:
-    print("Email valid")
+   user_email[user_email.index("@")+1] != "." and\
+   user_email.rindex(".") > user_email.index("@") and\
+   len(user_email[user_email.rindex(".")+1:]) >= 2:
+    print("True")
 else:
-    print("Email in unvalid")
-
-
+    print("False")
