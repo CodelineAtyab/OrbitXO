e95a39fe29049f5aa086e92163d95fb117631682
Haya-xxx <haya.6@outlook.com>
2025-05-25 15:32:21 +0400

    my shopping list

---
+++ examples/from_haya/shopping_list.py
@@ -0,0 +1,8 @@
+def shopping():
+    list_1 = ["milk", "bread", "eggs", "apples"]
+    list_2 = []
+    list_3 = ["bananas", "cereal", "bananas", "orange juice"]
+    return[len(list_1) , len(list_2) , len(list_3)]
+
+x=shopping()
+print(x)
