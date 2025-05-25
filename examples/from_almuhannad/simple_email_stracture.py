fcdefc310ca35d93da1b86ad69e8ba18966bc1d0
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-05-25 10:48:01 +0400

    still in progress

--- examples/from_almuhannad/simple_email_stracture.py
+++ examples/from_almuhannad/simple_email_stracture.py
@@ -1,4 +1,12 @@
-user_email = input("Enter your email: ").split("@")
+user_email = input("Enter your email: ")
 
-if user_email.count("@") == 1:
+if "@" in user_email and "." in user_email and\
+   user_email.index("@") > 0 and\
+   user_email.index("@") < len(user_email)-1 and\
+   user_email.index(".") > user_email.index("@") and\
+   len(user_email[user_email.rindex(".")+1]) > 2:
+    print("Email valid")
+else:
+    print("Email in unvalid")
+
 
