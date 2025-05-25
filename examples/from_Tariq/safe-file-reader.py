93c4c2f5130184f3094e6d8555bc5e2505e6cfbe
TSiyabi <tariqalsiyabi0077@gmail.com>
2025-05-25 12:50:33 +0400

    added missing exception

--- examples/from_Tariq/safe-file-reader.py
+++ examples/from_Tariq/safe-file-reader.py
@@ -6,5 +6,9 @@ try:
   with open(FILE_PATH, "r") as data_file:
     for line in data_file.readlines():
       print(line.strip())
+except FileNotFoundError:
+    print(f"Error: The file '{FILE_PATH}' was not found.")
+except PermissionError:
+    print(f"Error: Permission denied when trying to read the file '{FILE_PATH}'.")
 except Exception as ex_obj:
   print(f"Something went wrong with the file: {FILE_PATH}", ex_obj)
