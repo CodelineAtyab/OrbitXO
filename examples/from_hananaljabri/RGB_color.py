78b10e6a9a60d6b5f97eaf22a57b05ed5ae58204
Hanan170 <han.aljabri@gmail.com>
2025-05-25 09:32:25 +0400

    updated

--- examples/from_hananaljabri/RGB_color.py
+++ examples/from_hananaljabri/RGB_color.py
@@ -1,6 +1,10 @@
+color_list = []
+
 def num_to_hex(n):
     hex_dig = "0123456789ABCDEF"
-    return hex_dig[n // 16] + hex_dig[n % 16]
+    value1 = n // 16
+    value2 = n % 16
+    return hex_dig[value1] + hex_dig[value2]
 
 def RGB_to_hex(color):
     r, g, b = color
@@ -15,62 +19,61 @@ def palette_color(palette):
         print("4. Color matching")
         print("5. Exit")
 
-        choice = input("Choose an option from the above: ")
+        choose = input("Choose an option from the above: ")
 
-        if choice == "1":
-            try:
-                r = int(input("Enter red color value (0 - 255): "))
-                g = int(input("Enter green color value (0 - 255): "))
-                b = int(input("Enter blue color value (0 - 255): "))
-                if 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255:
-                    palette.append((r, g, b))
-                    print(f"Color {(r, g, b)} added.")
-                else:
-                    print("Invalid RGB values!")
-            except ValueError:
-                print("Please enter numbers only.")
+        if choose == "1":
+            r = int(input("Enter red color value (0 - 255): "))
+            g = int(input("Enter green color value (0 - 255): "))
+            b = int(input("Enter blue color value (0 - 255): "))
 
-        elif choice == "2":
+            if (0 <= r <= 255) and (0 <= g <= 255) and (0 <= b <= 255):
+                palette.append((r, g, b))
+                print(f"Color {(r, g, b)} added.")
+            else:
+                print("Invalid RGB values!")
+
+        elif choose == "2":
             if not palette:
                 print("No colors yet!")
             else:
                 print("Color Palette:")
-                for i, color in enumerate(palette, start=1):
-                    print(f"{i}. {color}")
+                for i in range(len(palette)):
+                    print(f"{i + 1}. {palette[i]}")
 
-        elif choice == "3":
+        elif choose == "3":
             if not palette:
                 print("No colors to convert.")
             else:
-                try:
-                    index = int(input("Enter color index (1, 2, ...): "))
-                    if 1 <= index <= len(palette):
-                        color = palette[index - 1]
-                        print(f"Hex code for {color} is {RGB_to_hex(color)}")
-                    else:
-                        print("Invalid index!")
-                except ValueError:
-                    print("Please enter a valid number.")
+                index = int(input("Enter color index (1, 2, ...): "))
+                if 1 <= index <= len(palette):
+                    r, g, b = palette[index - 1]
+                    print(f"Hex code for {(r, g, b)} is {RGB_to_hex((r, g, b))}")
+                else:
+                    print("Invalid index!")
 
-        elif choice == "4":
+        elif choose == "4":
             if not palette:
                 print("Palette is empty.")
             else:
-                try:
-                    r = int(input("Enter red value: "))
-                    g = int(input("Enter green value: "))
-                    b = int(input("Enter blue value: "))
-                    closest = min(palette, key=lambda c: (r - c[0])**2 + (g - c[1])**2 + (b - c[2])**2)
-                    print(f"The closest color to ({r}, {g}, {b}) is {closest}")
-                except ValueError:
-                    print("Please enter valid numbers.")
+                r = int(input("Enter red value: "))
+                g = int(input("Enter green value: "))
+                b = int(input("Enter blue value: "))
+                closest = None
+                min_distance = None
 
-        elif choice == "5":
-            print("Exiting... bye bye.")
+                for color in palette:
+                    cr, cg, cb = color
+                    dist = (r - cr) ** 2 + (g - cg) ** 2 + (b - cb) ** 2
+                    if min_distance is None or dist < min_distance:
+                        min_distance = dist
+                        closest = color
+
+                print(f"The closest color to ({r}, {g}, {b}) is {closest}")
+
+        elif choose == "5":
+            print("Exiting, Goodbye")
             break
         else:
-            print("Invalid choice, please try again.")
+            print("Invalid choice, please try again :)")
 
-# Start the program
-color_list = []
 palette_color(color_list)
