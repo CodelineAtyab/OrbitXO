21c35424047ef0fe1f3785be033b3200ebe47120
Ikhlas Al Khusaibi <ekhlasalkhusaibi@gmail.com>
2025-05-25 10:23:36 +0400

    Adding shopping list task

---
+++ examples/from_ikhlas/shopping_list.py
@@ -0,0 +1,7 @@
+def shopping_items(sh_lst):
+    return len(sh_lst)
+print(shopping_items(["milk", "bread", "eggs", "apples"]))
+
+print(shopping_items([])) #Empty list
+
+print(shopping_items(["bananas", "cereal", "bananas", "orange juice"]))
