847bdfdcca3b3e02fab40108f0003ed699ece1ed
Haya-xxx <haya.6@outlook.com>
2025-05-26 09:40:31 +0400

    fix the code

--- examples/from_haya/shopping_list.py
+++ examples/from_haya/shopping_list.py
@@ -1,8 +1,16 @@
-def shopping():
-    list_1 = ["milk", "bread", "eggs", "apples"]
-    list_2 = []
-    list_3 = ["bananas", "cereal", "bananas", "orange juice"]
-    return[len(list_1) , len(list_2) , len(list_3)]
+def shopping_length(shopping_list):
+    return len(shopping_list)
 
-x=shopping()
-print(x)
+list_1 = ["milk", "bread", "eggs", "apples"]
+list_2 = []
+list_3 = ["bananas", "cereal", "bananas", "orange juice"]
+
+print(shopping_length(list_1))
+print(shopping_length(list_2))
+print(shopping_length(list_3))
+
+
+
+
+
+
