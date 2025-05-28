b8edf29b3fde4b046f9925b1a64e9dc08e400bf8
Sulaiman <Suleiman201804013@gmail.com>
2025-05-28 15:46:46 +0400

    completed my "hard coded" tiktaktoe code, hope I didn't miss anything

--- examples/from_sulaiman/tiktaktoe_from_scratch.py
+++ examples/from_sulaiman/tiktaktoe_from_scratch.py
@@ -1,12 +1,22 @@
 board = [
-    ["X", "O", "O"],
-    ["O", "X", "X"],
-    ["X", "X", "O"]
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
     ]
 
-def play_a_turn():
-    
-    pass
+def play_a_turn(pos1, pos2, symbol):
+    board[pos1][pos2] = symbol
+    print(str(board[0]) + "\n" + str(board[1]) + "\n" + str(board[2]) + "\n")
 
-player_turn = ["11", "00", "21", "20", "12"]
-bot_turn = ["10", "22", "01", "02"]
+player_pos = ["11", "00", "21", "20", "12"]
+bot_pos = ["10", "22", "01", "02"]
+player_symbol = "X"
+bot_symbol = "O"
+
+for index in range(len(player_pos)):
+    try:
+        play_a_turn(int(player_pos[index][0]), int(player_pos[index][1]), player_symbol)
+
+        play_a_turn(int(bot_pos[index][0]), int(bot_pos[index][1]), bot_symbol)
+    except IndexError:
+        pass
