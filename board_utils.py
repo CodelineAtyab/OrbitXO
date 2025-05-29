ab1d0fcc11343b4c0208907249f30352499c787e
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-29 13:45:18 +0400

    Updated logic

--- board_utils.py
+++ board_utils.py
@@ -1,14 +1,24 @@
 import time
 
-def make_move(board, sel_row, sel_col, symbol):
-  # Validation
-  if (sel_row >= 0 and sel_row <= 2) and (sel_col >= 0 and sel_col <= 2):
-    # User 1 Makes a move
-    board[sel_row][sel_col] = symbol
-    print(f"User 1 Move: {sel_row} - {sel_col}")
 
-    # Print the entire board
-    for row in board:
-      print(row)
+def make_move(board, sel_row, sel_col, symbol):
+  move_successful = False
+  if board[sel_row][sel_col] == "-":
+    if (sel_row >= 0 and sel_row <= 2) and (sel_col >= 0 and sel_col <= 2):
+      # User 1 Makes a move
+      board[sel_row][sel_col] = symbol
+      move_successful = True
+      print(f"User 1 Move: {sel_row} - {sel_col}")
+
+      # Print the entire board
+      for row in board:
+        print(row)
+  else:
+    print(f"Unable to make a move at {sel_row}-{sel_col} with {symbol}")
     
-    # time.sleep(1)
+  time.sleep(1)
+  return move_successful
+
+
+def is_board_filled(board, empty_box_symbol):
+  return empty_box_symbol in str(board)
--- main_app.py
+++ main_app.py
@@ -1,17 +1,25 @@
 import board_utils
 
+
+EMPTY_SYMBOL = "-"
+
 board = [
-  ["-", "-", "-"],  # 00 01 02
-  ["-", "-", "-"],  # 10 11 12
-  ["-", "-", "-"]   # 20 21 22
+  [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
+  [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
+  [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
 ]
 
 all_positions = ["00", "01", "02", "10", "11", "12", "20", "21", "22"]
 
 user_1_symbol = "X"
 bot_symbol = "O"
-user_1_inp_sequence = ["00", "11", "12", "01", "20"]
-bot_inp_sequence = ["02", "22", "10", "21"]
+user_1_inp_sequence = ["00", "11", "12", "01", "20", "00"]
+bot_inp_sequence = ["02", "22", "10", "21", "22"]
+
+user_move_index = 0
+bot_move_index = 0
+
+is_players_move = True
 
 # Process
 for index in range(0, len(user_1_inp_sequence)):
@@ -19,17 +27,29 @@ for index in range(0, len(user_1_inp_sequence)):
   Step 1 - User Makes a Move
   """
   try:
-    board_utils.make_move(board=board, 
-                          sel_row=int(user_1_inp_sequence[index][0]), 
-                          sel_col=int(user_1_inp_sequence[index][1]), 
-                          symbol=user_1_symbol)
+    if is_players_move:
+      board_utils.make_move(board=board, 
+                            sel_row=int(user_1_inp_sequence[index][0]), 
+                            sel_col=int(user_1_inp_sequence[index][1]), 
+                            symbol=user_1_symbol)
+      is_players_move = False
+    
+    if not is_players_move:
+      """
+      Step 2 - Bot Makes a Move
+      """
+      is_move_successfully_made = False
+      while not is_move_successfully_made:
+        is_move_successfully_made = board_utils.make_move(board=board, 
+                                                          sel_row=int(bot_inp_sequence[index][0]), 
+                                                          sel_col=int(bot_inp_sequence[index][1]), 
+                                                          symbol=bot_symbol)
+        if not is_move_successfully_made:
+          pass
+        else:
+          is_players_move = True
+    
+    print(board_utils.is_board_filled(board, EMPTY_SYMBOL))
 
-    """
-    Step 2 - Bot Makes a Move
-    """
-    board_utils.make_move(board=board, 
-                          sel_row=int(bot_inp_sequence[index][0]), 
-                          sel_col=int(bot_inp_sequence[index][1]), 
-                          symbol=bot_symbol)
   except IndexError:
     print("Board is already filled. Thanks!")
