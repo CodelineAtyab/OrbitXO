a1731d7a9deed32c9312654411e16b1f20be16b4
Muzna-Adil-Almamari <muznaadilalmamari@gmail.com>
2025-06-03 16:31:53 +0400

    Feature/86etj3p4m target sum (#228)
    * & C:/Users/admin/AppData/Local/Microsoft/WindowsApps/python3.13.exe c:/Users/admin/Documents/OrbitXO/examples/from_Muzna/target_sum.py
    
    * target-sum

---
+++ examples/from_Muzna/target_sum.py
@@ -0,0 +1,14 @@
+def find_target_sum(nums, target):
+    seen = set()
+    trgt_set=set()
+    for i in nums:
+        div = target - i
+        if div in seen:
+            trgt_set.add(tuple((i,div)))
+        seen.add(i)
+
+    return list(trgt_set)
+
+print(find_target_sum([2, 7, 11, 15, -2], 9))
+print(find_target_sum([3, 2, 4, 3], 6))
+print(find_target_sum([1, 5, 3, 8], 12))
