185f17e35d2c158e44b4ec64cfefc6c4a19226ec
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-06-11 09:45:28 +0400

    Done starting the new tic tac toe battle

---
+++ examples/from_almuhannad/game/starting_new_tic_tac_toe_battle.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import uuid
+import uvicorn
+import util
+
+games = {}
+
+empty_symbol = "_"
+
+app = FastAPI()
+
+@app.post("/board")
+def create_board():
+    board_id = str(uuid.uuid4())
+
+    games[board_id] = {
+        "board": util.board(),
+        "active_player": "X"
+    }
+    return {"board_id": board_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
