bc50f075b7539d1500668fbe6c41087e68dbbf12
aroobaalfarsi <aroobaalfarsi@gmail.com>
2025-06-12 13:07:32 +0400

    ttt

--- examples/from_arooba/Game_Creator.py
+++ examples/from_arooba/Game_Creator.py
@@ -9,20 +9,15 @@ app= FastAPI()
 
 def board():
       new_game_id = str(uuid.uuid4())
-
-    # Make a new board and set the starting player
       new_game = {
-        "board": util.board(),         # Create a 3x3 empty board
-        "active_player": "X"           # Set "X" as the player who starts
+        "board": util.board(),         
+        "active_player": "X"           
     }
 
-    # Save the game in the board_games dictionary using the ID
       util.board_games[new_game_id] = new_game
 
-    # Send back the game ID so the player can use it later
       return {"board_id": new_game_id}
         
        
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0" ,port=8888)
