da037aa77dffec13a185b3763e8f31021f500d80
aroobaalfarsi <aroobaalfarsi@gmail.com>
2025-06-12 11:38:02 +0400

    Removed from_Fatma folder

--- examples/from_Fatma/File_reader.py
+++
@@ -1,9 +0,0 @@
-File_Name= input("Enter the file name: ")
-try:
-    with open (File_Name,"r") as file:
-        data=file.read()
-
-except FileNotFoundError:
-    print("File not found.")
-except PermissionError:
-    print("You don't have permission to read this file.")        
--- examples/from_Fatma/message_formatter.py
+++
@@ -1,5 +0,0 @@
-def text_message(message):
-    if len(message)<= 280:
-        return message
-    else:
-        return message[:277] + "..."
