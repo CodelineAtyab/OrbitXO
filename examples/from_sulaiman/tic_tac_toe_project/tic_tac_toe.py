2e4690d906d396d4847a345493772a07316abaf2
Sulaiman <Suleiman201804013@gmail.com>
2025-06-15 09:29:22 +0400

    now we can track who's move it is in a board

--- examples/from_sulaiman/tic_tac_toe_project/tic_tac_toe.py
+++ examples/from_sulaiman/tic_tac_toe_project/tic_tac_toe.py
@@ -7,10 +7,24 @@ app = FastAPI()
 @app.post("/board")
 def create_board():
     board_id = str(uuid.uuid4())
-    utils.tictactoe_games[board_id] = {
-        "board": utils.board,
-    }
+    utils.tictactoe_games[board_id] = {"board": utils.empty_board()
+                                        , "player_turn": "1"
+                                        , "player_1_symbol": "X"
+                                        , "player_2_symbol": "O"
+                                        , "game_over": False}
     return {"board_id": board_id}
-# @app.get("/board/{board_id}")
 
-uvicorn.run(app, host="0.0.0.0", port=8567)
+@app.get("/boards")
+def get_boards():
+    return utils.display_boards()
+
+@app.get("/board/{board_id}")
+def get_board(board_id: str):
+    return utils.display_board(board_id)
+
+@app.post("/board/{board_id}/move")
+def turn_tracker(board_id):
+    return utils.turn_tracker(board_id)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8567)
--- examples/from_sulaiman/tic_tac_toe_project/utils.py
+++ examples/from_sulaiman/tic_tac_toe_project/utils.py
@@ -2,6 +2,26 @@ tictactoe_games = {}
 
 EMPTY_SYMBOL = "-"
 
-board = [[EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
-         [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
-         [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL]]
+def empty_board():
+    return [[EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
+            [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL],
+            [EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL]]
+
+def display_boards():
+    return {"boards": list(tictactoe_games.keys())}
+
+def display_board(board_id):
+    if board_id in tictactoe_games:
+        return {"board":tictactoe_games[board_id]}
+    else:
+        return {"error":"Board not found"}
+    
+def turn_tracker(board_id):
+    if board_id not in tictactoe_games:
+        return {"error": "Board not found"}
+    if tictactoe_games[board_id]["player_turn"] == "1":
+        symbol = tictactoe_games[board_id]["player_1_symbol"]
+    else:
+        symbol = tictactoe_games[board_id]["player_2_symbol"]
+    return {"player_turn": tictactoe_games[board_id]["player_turn"], 
+            "symbol": symbol}
