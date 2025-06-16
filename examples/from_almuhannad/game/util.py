ee1f26991c953835153c76b34e94133ff2bad144
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-06-16 12:15:48 +0400

    Done with board_viewer

--- examples/from_almuhannad/game/util.py
+++ examples/from_almuhannad/game/util.py
@@ -1,9 +1,18 @@
+import uuid
 games = {}
 
 empty_symbol = "_"
 
 def board():
-    return [[empty_symbol, empty_symbol, empty_symbol], 
+    board_id = str (uuid.uuid4())
+    games[board_id] = [
+          [empty_symbol, empty_symbol, empty_symbol], 
             [empty_symbol, empty_symbol, empty_symbol],
             [empty_symbol, empty_symbol, empty_symbol]
             ]
+    return board_id
+
+def get_board(board_id):
+    if board_id in games:
+        return games[board_id]
+    return None
