b8f531f2b388a9a7997435af11bafc3c392bff58
HananBlc9 <hanan.balucci@gmail.com>
2025-07-01 10:12:44 +0400

    updated email re

--- examples/from_Hanan_Balushi/regexEmailVerifier.py
+++ examples/from_Hanan_Balushi/regexEmailVerifier.py
@@ -3,7 +3,7 @@ import re
 file = open("examples/from_Hanan_Balushi/emails.txt", "r")
 for line in file.readlines():
     email = line.strip()
-    if re.search(r"^[a-zA-Z0-9_+-.]{2,}@[a-zA-Z]{2,}[\.[a-zA-Z]{2,}]*?\.[a-zA-Z]{2,}$", email):
+    if re.search(r"^[a-zA-Z0-9_.+-]{2,}@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$", email):
         print(f"Email: {email} = Valid Email!")
     else:
         print(f"Email: {email} = Invalid Email!!!") #if beginning is correct but there is no domain print invalid
